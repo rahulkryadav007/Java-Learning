b0d5f760a04b335b43019a7ac67b42116d466b91
Rahul Kumar Yadav <120457022+rahulkryadav007@users.noreply.github.com>
2026-08-28 00:18:07 +0530

    Add for loop example

---
+++ 06-Loops/ForLoopExample.java
@@ -0,0 +1,11 @@
+package com.loop;
+
+public class ForLoopExample {
+    public static void main(String[] args) {
+        // for loop: initialization -> condition -> update
+        for (int i = 1; i <= 5; i++) {
+            // This statement runs once for every iteration.
+            System.out.println("Number: " + i);
+        }
+    }
+}
