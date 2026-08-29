021237294ccf05302d337f53ca1fc8e13a2aeb4e
Rahul Kumar Yadav <120457022+rahulkryadav007@users.noreply.github.com>
2026-08-29 23:58:37 +0530

    Add Hinglish comments to inheritance basic example

--- 08-OOP/06-Inheritance/src/com/javalearning/oop/inheritance/BasicExample.java
+++ 08-OOP/06-Inheritance/src/com/javalearning/oop/inheritance/BasicExample.java
@@ -1,21 +1,23 @@
 package com.javalearning.oop.inheritance;
 
+// Inheritance: child class parent ke accessible members reuse kar sakti hai.
 class Animal {
     void eat() {
-        System.out.println("Animal is eating");
+        System.out.println("Animal is eating"); // Parent ka method.
     }
 }
 
-class Dog extends Animal {
+class Dog extends Animal { // Dog IS-A Animal; extends se inheritance ho rahi hai.
     void bark() {
-        System.out.println("Dog is barking");
+        System.out.println("Dog is barking"); // Dog ka apna behavior.
     }
 }
 
 public class BasicExample {
     public static void main(String[] args) {
+        // Dog object bana rahe hain.
         Dog dog = new Dog();
-        dog.eat();
-        dog.bark();
+        dog.eat();  // Parent ka inherited method call.
+        dog.bark(); // Child ka method call.
     }
 }
