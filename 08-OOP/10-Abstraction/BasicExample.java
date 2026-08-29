802ebaec06e04589217646eb03a369922e55d21e
Rahul Kumar Yadav <120457022+rahulkryadav007@users.noreply.github.com>
2026-08-29 23:42:50 +0530

    Add abstraction runnable example

---
+++ 08-OOP/10-Abstraction/BasicExample.java
@@ -0,0 +1,22 @@
+abstract class Vehicle {
+    abstract void start();
+
+    void stop() {
+        System.out.println("Vehicle stopped");
+    }
+}
+
+class Car extends Vehicle {
+    @Override
+    void start() {
+        System.out.println("Car starts");
+    }
+}
+
+public class BasicExample {
+    public static void main(String[] args) {
+        Vehicle vehicle = new Car();
+        vehicle.start();
+        vehicle.stop();
+    }
+}
