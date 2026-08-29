68f0012e5fc4101cecbf97f8984261c83e7c8022
Rahul Kumar Yadav <120457022+rahulkryadav007@users.noreply.github.com>
2026-08-29 23:59:00 +0530

    Add Hinglish comments to overriding basic example

--- 08-OOP/08-Method-Overriding/src/com/javalearning/oop/overriding/BasicExample.java
+++ 08-OOP/08-Method-Overriding/src/com/javalearning/oop/overriding/BasicExample.java
@@ -1,11 +1,13 @@
 package com.javalearning.oop.overriding;
 
+// Parent class ka common behavior.
 class Animal {
     void sound() {
         System.out.println("Animal makes a sound");
     }
 }
 
+// Dog parent ke sound() ko apne behavior se replace kar raha hai.
 class Dog extends Animal {
     @Override
     void sound() {
@@ -15,7 +17,10 @@ class Dog extends Animal {
 
 public class BasicExample {
     public static void main(String[] args) {
+        // Reference Animal ka hai, actual object Dog ka hai.
         Animal animal = new Dog();
+
+        // Runtime par Dog ka overridden sound() execute hoga.
         animal.sound();
     }
 }
