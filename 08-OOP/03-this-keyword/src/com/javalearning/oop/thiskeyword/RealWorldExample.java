610cc4b6644f433bc786418026f6b5032f6d2bbb
Rahul Kumar Yadav <120457022+rahulkryadav007@users.noreply.github.com>
2026-08-29 23:58:03 +0530

    Add Hinglish comments to this keyword real world example

--- 08-OOP/03-this-keyword/src/com/javalearning/oop/thiskeyword/RealWorldExample.java
+++ 08-OOP/03-this-keyword/src/com/javalearning/oop/thiskeyword/RealWorldExample.java
@@ -1,21 +1,25 @@
 package com.javalearning.oop.thiskeyword;
 
+// Product ek real-world object ko represent karta hai.
 class Product {
     String name;
     double price;
 
     Product(String name, double price) {
+        // this current Product object ko refer karta hai.
         this.name = name;
         this.price = price;
     }
 
     void showProduct() {
+        // Current product ki details print kar rahe hain.
         System.out.println(name + " - ₹" + price);
     }
 }
 
 public class RealWorldExample {
     public static void main(String[] args) {
+        // Product object create karte waqt constructor values set karega.
         Product product = new Product("Laptop", 60000);
         product.showProduct();
     }
