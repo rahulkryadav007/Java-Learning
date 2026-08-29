c2f2be9e14d4ed34ef669d4a88c3acd45ad5ac76
Rahul Kumar Yadav <120457022+rahulkryadav007@users.noreply.github.com>
2026-08-29 23:57:57 +0530

    Add Hinglish comments to this keyword basic example

--- 08-OOP/03-this-keyword/src/com/javalearning/oop/thiskeyword/BasicExample.java
+++ 08-OOP/03-this-keyword/src/com/javalearning/oop/thiskeyword/BasicExample.java
@@ -1,20 +1,24 @@
 package com.javalearning.oop.thiskeyword;
 
 class Student {
-    String name;
+    String name; // Instance variable: har object ka apna name.
 
     Student(String name) {
+        // this.name current object ke field ko refer karta hai.
+        // name constructor ka parameter hai.
         this.name = name;
     }
 
     void show() {
+        // this.name current Student object ka name print karega.
         System.out.println("Name: " + this.name);
     }
 }
 
 public class BasicExample {
     public static void main(String[] args) {
+        // Constructor ko name dekar Student object bana rahe hain.
         Student student = new Student("Rahul");
-        student.show();
+        student.show(); // Current object ka data print hoga.
     }
 }
