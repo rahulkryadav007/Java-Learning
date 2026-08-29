3de5f892b65b6df9665b364541e39844c201fc0e
Rahul Kumar Yadav <120457022+rahulkryadav007@users.noreply.github.com>
2026-08-29 23:59:27 +0530

    Add Hinglish comments to abstraction real world example

--- 08-OOP/10-Abstraction/src/com/javalearning/oop/abstraction/RealWorldExample.java
+++ 08-OOP/10-Abstraction/src/com/javalearning/oop/abstraction/RealWorldExample.java
@@ -1,16 +1,18 @@
 package com.javalearning.oop.abstraction;
 
+// User ko notification ka WHAT pata hona chahiye, implementation detail nahi.
 abstract class Notification {
-    abstract void send(String message);
+    abstract void send(String message); // Child class sending ka HOW define karegi.
 
     void log(String message) {
-        System.out.println("LOG: " + message);
+        System.out.println("LOG: " + message); // Common logging logic.
     }
 }
 
 class EmailNotification extends Notification {
     @Override
     void send(String message) {
+        // Email notification ka actual implementation yahan hai.
         log(message);
         System.out.println("Sending email: " + message);
     }
@@ -18,6 +20,7 @@ class EmailNotification extends Notification {
 
 public class RealWorldExample {
     public static void main(String[] args) {
+        // Abstract type ke through concrete EmailNotification use kar rahe hain.
         Notification notification = new EmailNotification();
         notification.send("Welcome to the application");
     }
