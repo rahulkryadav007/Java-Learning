bc5f50a185b29d830a564f1613f23f893861bed3
Rahul Kumar Yadav <120457022+rahulkryadav007@users.noreply.github.com>
2026-08-29 23:58:09 +0530

    Add Hinglish comments to static basic example

--- 08-OOP/04-static/src/com/javalearning/oop/staticdemo/BasicExample.java
+++ 08-OOP/04-static/src/com/javalearning/oop/staticdemo/BasicExample.java
@@ -1,13 +1,15 @@
 package com.javalearning.oop.staticdemo;
 
 class Student {
+    // static variable class ka shared data hai; har object ke liye separate copy nahi.
     static String college = "ABC College";
-    String name;
+    String name; // Instance variable: har Student ka alag name.
 
     Student(String name) {
         this.name = name;
     }
 
+    // static method ko object banaye bina class name se call kar sakte hain.
     static void showCollege() {
         System.out.println("College: " + college);
     }
@@ -15,9 +17,14 @@ class Student {
 
 public class BasicExample {
     public static void main(String[] args) {
+        // Class name se static method call ho raha hai.
         Student.showCollege();
+
+        // Do alag Student objects create kar rahe hain.
         Student a = new Student("Rahul");
         Student b = new Student("Aman");
+
+        // Name alag hai, lekin college static hone ki wajah se shared hai.
         System.out.println(a.name + " and " + b.name + " study at " + Student.college);
     }
 }
