de681016e6ca12da1a1c9a6644ebcc9582ffee28
Rahul Kumar Yadav <120457022+rahulkryadav007@users.noreply.github.com>
2026-08-29 23:59:32 +0530

    Add Hinglish comments to abstract class basic example

--- 08-OOP/11-Abstract-Class/src/com/javalearning/oop/abstractclass/BasicExample.java
+++ 08-OOP/11-Abstract-Class/src/com/javalearning/oop/abstractclass/BasicExample.java
@@ -1,28 +1,31 @@
 package com.javalearning.oop.abstractclass;
 
+// Abstract class ka direct object nahi bana sakte.
 abstract class Animal {
     Animal() {
+        // Child object create hone par parent constructor pehle chalega.
         System.out.println("Animal constructor");
     }
 
-    abstract void sound();
+    abstract void sound(); // Child ko sound() implement karna hoga.
 
     void eat() {
-        System.out.println("Animal eats");
+        System.out.println("Animal eats"); // Concrete/common method.
     }
 }
 
 class Dog extends Animal {
     @Override
     void sound() {
-        System.out.println("Dog barks");
+        System.out.println("Dog barks"); // Dog ka specific implementation.
     }
 }
 
 public class BasicExample {
     public static void main(String[] args) {
+        // Dog ka object ban raha hai; parent constructor bhi call hoga.
         Dog dog = new Dog();
-        dog.sound();
-        dog.eat();
+        dog.sound(); // Implemented abstract method.
+        dog.eat();   // Inherited concrete method.
     }
 }
