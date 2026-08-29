589981ef4d10eeb44c7b40647fbd61f26fe36a17
Rahul Kumar Yadav <120457022+rahulkryadav007@users.noreply.github.com>
2026-08-29 23:58:48 +0530

    Add Hinglish comments to overloading basic example

--- 08-OOP/07-Method-Overloading/src/com/javalearning/oop/overloading/BasicExample.java
+++ 08-OOP/07-Method-Overloading/src/com/javalearning/oop/overloading/BasicExample.java
@@ -1,14 +1,25 @@
 package com.javalearning.oop.overloading;
 
+// Same method name, different parameters = method overloading.
 class Calculator {
-    int add(int a, int b) { return a + b; }
-    int add(int a, int b, int c) { return a + b + c; }
-    double add(double a, double b) { return a + b; }
+    int add(int a, int b) {
+        return a + b; // 2 integers add kar rahe hain.
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c; // 3 integers add kar rahe hain.
+    }
+
+    double add(double a, double b) {
+        return a + b; // double values ke liye overloaded method.
+    }
 }
 
 public class BasicExample {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
+
+        // Compiler arguments dekhkar correct add() method choose karega.
         System.out.println(calculator.add(10, 20));
         System.out.println(calculator.add(10, 20, 30));
         System.out.println(calculator.add(10.5, 20.5));
