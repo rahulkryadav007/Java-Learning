b307cc84c14950efb706f39d1b75d321b4e58ed4
Rahul Kumar Yadav <120457022+rahulkryadav007@users.noreply.github.com>
2026-08-29 23:42:55 +0530

    Add abstract class runnable example

---
+++ 08-OOP/11-Abstract-Class/BasicExample.java
@@ -0,0 +1,26 @@
+abstract class Animal {
+    Animal() {
+        System.out.println("Animal constructor");
+    }
+
+    abstract void sound();
+
+    void eat() {
+        System.out.println("Animal eats");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class BasicExample {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.sound();
+        dog.eat();
+    }
+}
