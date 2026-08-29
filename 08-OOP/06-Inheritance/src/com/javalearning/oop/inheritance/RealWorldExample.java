82d3a09eaaaa11bcbc1681489a79be815c90989d
Rahul Kumar Yadav <120457022+rahulkryadav007@users.noreply.github.com>
2026-08-29 23:58:43 +0530

    Add Hinglish comments to inheritance real world example

--- 08-OOP/06-Inheritance/src/com/javalearning/oop/inheritance/RealWorldExample.java
+++ 08-OOP/06-Inheritance/src/com/javalearning/oop/inheritance/RealWorldExample.java
@@ -1,31 +1,33 @@
 package com.javalearning.oop.inheritance;
 
+// Common employee data/behavior parent class mein rakha hai.
 class Employee {
-    protected String name;
+    protected String name; // Child class isse access kar sakti hai.
 
     Employee(String name) {
-        this.name = name;
+        this.name = name; // Current employee ka name set ho raha hai.
     }
 
     void work() {
-        System.out.println(name + " is working");
+        System.out.println(name + " is working"); // Common behavior.
     }
 }
 
+// Developer Employee ka specialized type hai.
 class Developer extends Employee {
     Developer(String name) {
-        super(name);
+        super(name); // Parent constructor ko call karke name initialize kar rahe hain.
     }
 
     void writeCode() {
-        System.out.println(name + " is writing Java code");
+        System.out.println(name + " is writing Java code"); // Developer-specific behavior.
     }
 }
 
 public class RealWorldExample {
     public static void main(String[] args) {
         Developer developer = new Developer("Rahul");
-        developer.work();
-        developer.writeCode();
+        developer.work();      // Employee se inherited method.
+        developer.writeCode(); // Developer ka own method.
     }
 }
