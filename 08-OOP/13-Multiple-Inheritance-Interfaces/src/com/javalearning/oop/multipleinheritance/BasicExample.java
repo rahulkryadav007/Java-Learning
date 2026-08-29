fce202f271b3763e68c414fbc9ef9de7c020af3b
Rahul Kumar Yadav <120457022+rahulkryadav007@users.noreply.github.com>
2026-08-29 23:59:53 +0530

    Add Hinglish comments to multiple inheritance basic example

--- 08-OOP/13-Multiple-Inheritance-Interfaces/src/com/javalearning/oop/multipleinheritance/BasicExample.java
+++ 08-OOP/13-Multiple-Inheritance-Interfaces/src/com/javalearning/oop/multipleinheritance/BasicExample.java
@@ -1,5 +1,6 @@
 package com.javalearning.oop.multipleinheritance;
 
+// Java multiple classes ko extend nahi karne deta, but multiple interfaces implement kar sakte hain.
 interface Flyable {
     void fly();
 }
@@ -8,15 +9,21 @@ interface Swimmable {
     void swim();
 }
 
+// Duck dono capabilities ka contract follow kar raha hai.
 class Duck implements Flyable, Swimmable {
-    public void fly() { System.out.println("Duck flies"); }
-    public void swim() { System.out.println("Duck swims"); }
+    public void fly() {
+        System.out.println("Duck flies");
+    }
+
+    public void swim() {
+        System.out.println("Duck swims");
+    }
 }
 
 public class BasicExample {
     public static void main(String[] args) {
         Duck duck = new Duck();
-        duck.fly();
-        duck.swim();
+        duck.fly();  // Flyable contract ka implementation.
+        duck.swim(); // Swimmable contract ka implementation.
     }
 }
