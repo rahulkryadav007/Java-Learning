7e32e01b25d8d0d64b079496694e20a3fe28fc0c
Rahul Kumar Yadav <120457022+rahulkryadav007@users.noreply.github.com>
2026-08-29 23:57:52 +0530

    Add Hinglish comments to constructor real world example

--- 08-OOP/02-Constructor/src/com/javalearning/oop/constructor/RealWorldExample.java
+++ 08-OOP/02-Constructor/src/com/javalearning/oop/constructor/RealWorldExample.java
@@ -1,23 +1,27 @@
 package com.javalearning.oop.constructor;
 
+// Employee object ko create karte waqt required information de rahe hain.
 class Employee {
     int id;
     String name;
     String department;
 
+    // Constructor object ki initial state set kar raha hai.
     Employee(int id, String name, String department) {
-        this.id = id;
-        this.name = name;
-        this.department = department;
+        this.id = id;                 // Current employee ka id.
+        this.name = name;             // Current employee ka name.
+        this.department = department; // Current employee ka department.
     }
 
     void show() {
+        // Employee object ki information display kar rahe hain.
         System.out.println(id + " - " + name + " - " + department);
     }
 }
 
 public class RealWorldExample {
     public static void main(String[] args) {
+        // Employee create karte hi constructor call hoga.
         Employee employee = new Employee(101, "Rahul", "Java Backend");
         employee.show();
     }
